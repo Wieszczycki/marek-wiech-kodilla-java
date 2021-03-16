40cd0f67c8c9d199acbb1f4793d6645ec3469d77
Marek Wiech <wieszcz@data.pl>
2021-03-16 03:40:58 +0100

    17.2 zadanie - stworzenie encji listy zadań

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -0,0 +1,37 @@
+package com.kodilla.hibernate.tasklist.dao;
+
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class TaskListDaoTestSuite {
+
+    @Autowired
+    private TaskListDao taskListDao;
+    private static final String LISTNAME = "test1";
+    private static final String DESCRIPTION = "Test: TASKLISTS ";
+
+    @Test
+    void testTaskListDaoSave() {
+        //Given
+        TaskList taskList = new TaskList(LISTNAME, DESCRIPTION);
+
+        //When
+        taskListDao.save(taskList);
+
+        //Then
+        int id = taskList.getId();
+        Optional<TaskList> readTask = taskListDao.findById(id);
+        assertTrue(readTask.isPresent());
+
+        //CleanUp
+        taskListDao.deleteById(id);
+    }
+
+}
