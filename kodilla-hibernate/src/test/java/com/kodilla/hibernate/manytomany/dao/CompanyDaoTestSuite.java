5c4838b1b17f9e895dbce6407520a070866c7022
Marek Wiech <wieszcz@data.pl>
2021-03-26 16:22:58 +0100

    17.4.1 - zadanie: NamedQueries, na razie nie działa (failed to load ApplicationContext)

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
@@ -6,13 +6,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class CompanyDaoTestSuite {
 
     @Autowired
     private CompanyDao companyDao;
+    @Autowired
+    private EmployeeDao employeeDao;
 
     @Test
     void testSaveManyToMany() {
@@ -51,12 +55,60 @@ class CompanyDaoTestSuite {
         assertNotEquals(0, greyMatterId);
 
         //CleanUp
-        //try {
-        //    companyDao.deleteById(softwareMachineId);
-        //    companyDao.deleteById(dataMaestersId);
-        //    companyDao.deleteById(greyMatterId);
-        //} catch (Exception e) {
-        //    //do nothing
-        //}
+        try {
+            companyDao.deleteById(softwareMachineId);
+            companyDao.deleteById(dataMaestersId);
+            companyDao.deleteById(greyMatterId);
+        } catch (Exception e) {
+            //do nothing
+        }
+    }
+
+        @Test
+        void testNamedQueries() {
+            //Given
+            Employee johnSmith = new Employee("John", "Smith");
+            Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
+            Employee lindaKovalsky = new Employee("Linda", "Kovalsky");
+
+            Company softwareMachine = new Company("Software Machine");
+            Company dataMaesters = new Company("Data Maesters");
+            Company greyMatter = new Company("Grey Matter");
+
+            softwareMachine.getEmployees().add(johnSmith);
+            dataMaesters.getEmployees().add(stephanieClarckson);
+            dataMaesters.getEmployees().add(lindaKovalsky);
+            greyMatter.getEmployees().add(johnSmith);
+            greyMatter.getEmployees().add(lindaKovalsky);
+
+            johnSmith.getCompanies().add(softwareMachine);
+            johnSmith.getCompanies().add(greyMatter);
+            stephanieClarckson.getCompanies().add(dataMaesters);
+            lindaKovalsky.getCompanies().add(dataMaesters);
+            lindaKovalsky.getCompanies().add(greyMatter);
+
+            //When
+            companyDao.save(softwareMachine);
+            int softwareMachineId = softwareMachine.getId();
+            companyDao.save(dataMaesters);
+            int dataMaestersId = dataMaesters.getId();
+            companyDao.save(greyMatter);
+            int greyMatterId = greyMatter.getId();
+
+            List<Employee> lastName = employeeDao.retrieveEmployeeWithLastname("Smith");
+            List<Company> nameStartedWith = companyDao.retrieveCompanyNameLike("Sof");
+
+            //Then
+            assertEquals(2, lastName.size());
+            assertEquals(1, nameStartedWith.size());
+
+            //CleanUp
+            try {
+                companyDao.deleteById(softwareMachineId);
+                companyDao.deleteById(dataMaestersId);
+                companyDao.deleteById(greyMatterId);
+            } catch (Exception e) {
+                //do nothing
+            }
     }
 }
--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -82,4 +82,5 @@ public class TaskListDaoTestSuite {
         taskListDao.deleteById(id);
     }
 
+
 }
